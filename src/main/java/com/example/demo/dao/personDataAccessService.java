51600573d323ba9e5be7de85ba8d491932f8dd26
Rendy Febry <rendyfebry@gmail.com>
2020-01-07 17:33:36 +0700

    Implement SQL connection

--- src/main/java/com/example/demo/dao/personDataAccessService.java
+++ src/main/java/com/example/demo/dao/personDataAccessService.java
@@ -1,6 +1,8 @@
 package com.example.demo.dao;
 
 import com.example.demo.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,13 @@ import java.util.UUID;
 
 @Repository("postgres")
 public class personDataAccessService implements PersonDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public personDataAccessService(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
     @Override
     public int insertPerson(UUID id, Person person) {
         return 0;
@@ -16,7 +25,12 @@ public class personDataAccessService implements PersonDao {
 
     @Override
     public List<Person> selectAllPeople() {
-        return List.of(new Person(UUID.randomUUID(), "FROM POSTGRES DB"));
+        final String sql = "SELECT id, name FROM person";
+        return jdbcTemplate.query(sql, (resultSet, i) -> {
+            UUID id = UUID.fromString(resultSet.getString("id"));
+            String name = resultSet.getString("name");
+            return new Person(id, name);
+        });
     }
 
     @Override
